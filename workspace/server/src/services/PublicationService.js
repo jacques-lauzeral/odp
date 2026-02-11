206fa18dafd91e34019142bf3a8ce6d260533e17
milhourat <jacques.lauzeral@gmail.com>
2026-02-11 18:28:23 +0100

    Publication - Dummy Antora web-site.

--- workspace/server/src/services/PublicationService.js
+++ workspace/server/src/services/PublicationService.js
@@ -164,6 +164,7 @@ ui:
      */
     async generateDocx(editionId, userId) {
         // TODO: Implement
+        //
         throw new Error('Word document generation not yet implemented');
     }
 }
